898225f6fb4fe0d188d7a25c88ed827910b0421c
jenaalsup <jena.alsup@gmail.com>
2022-09-14 00:22:44 -0700

    clean up branches

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          website in progress
+          website in progress...
         </a>
       </header>
     </div>
