eeb4d1d2dd33541a80df2c8b06447464a355f3a0
jenaalsup <jena.alsup@gmail.com>
2025-06-03 00:01:39 -0700

    update homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,8 @@ function Home() {
     <div className="home-content">
       <h3>hi, i'm jena. currently:</h3>
       <ul>
-        <li>taking a break from my undergraduate studies to work on <a href="https://yuzu.health/" target="_blank" rel="noopener noreferrer">health insurance</a></li>
+        <li>building ai search @ <a href="https://exa.ai/" target="_blank" rel="noopener noreferrer">exa</a></li>
+        <li>understanding early human embryos @ <a href="https://zernickagoetzlab.com/" target="_blank" rel="noopener noreferrer">zernicka-goetz lab</a></li>
         <li>thinking about how to solve poverty, climate, and cities</li>
         <li>learning about ai, full-stack development, and plant-based solutions</li>
         <li>reading <a href="https://www.goodreads.com/book/show/108428.Are_Prisons_Obsolete_" target="_blank" rel="noopener noreferrer">angela davis</a>, <a href="http://www.paulgraham.com/articles.html" target="_blank" rel="noopener noreferrer">paul graham</a>, and <a href="https://www.goodreads.com/book/show/122765395-elon-musk" target="_blank" rel="noopener noreferrer">walter isaacson</a></li>
@@ -15,6 +16,7 @@ function Home() {
       
       <h3>formerly:</h3>
       <ul>
+        <li>automating health plan administration @ <a href="https://yuzu.health/" target="_blank" rel="noopener noreferrer">yuzu health</a></li>
         <li>building security tools <span style={{fontStyle: 'italic'}}>with</span> ai @ <a href="https://www.apple.com/" target="_blank" rel="noopener noreferrer">apple</a></li>
         <li>protecting government communications <span style={{fontStyle: 'italic'}}>from</span> ai @ <a href="https://www.paragonfellowship.org/" target="_blank" rel="noopener noreferrer">paragon policy fellowship</a></li>
         <li>secretary things (fighting admin) @ <a href="https://sites.google.com/site/arccaltech" target="_blank" rel="noopener noreferrer">caltech arc</a></li>
