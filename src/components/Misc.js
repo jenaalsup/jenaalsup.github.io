225f85716bdf47ce429936e121f70609f087e07a
jenaalsup <jena.alsup@gmail.com>
2025-07-12 19:29:05 -0700

    update misc

--- src/components/Misc.js
+++ src/components/Misc.js
@@ -152,6 +152,7 @@ function Misc() {
         <li><a href="https://www.paulgraham.com/cities.html" target="_blank" rel="noopener noreferrer">cities and ambition</a> by paul graham</li>
         <li><a href="https://ambrook.com/research/legislation/school-milk-soy-lactose-intolerance?utm_source=Ambrook+Research+Newsletter&utm_campaign=3f184d5d07-EMAIL_CAMPAIGN_2023_06_26_02_47_COPY_11&utm_medium=email&utm_term=0_-d85bfec4c4-%5BLIST_EMAIL_ID%5D" target="_blank" rel="noopener noreferrer">dairy in public schools</a></li>
         <li><a href="https://www.chriskuang.com/civictech" target="_blank" rel="noopener noreferrer">civic tech resources</a></li>
+        <li><a href="https://www.matthewsiu.com/pathfinder" target="_blank" rel="noopener noreferrer">pathfinder</a></li>
       </ul>
       
       <br></br>
