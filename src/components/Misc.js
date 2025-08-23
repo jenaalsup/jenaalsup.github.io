5527498ec090bd7074da88a6ae839c1fd965a869
jenaalsup <jena.alsup@gmail.com>
2025-08-23 01:11:47 -0700

    update misc

--- src/components/Misc.js
+++ src/components/Misc.js
@@ -153,6 +153,7 @@ function Misc() {
         <li><a href="https://ambrook.com/research/legislation/school-milk-soy-lactose-intolerance?utm_source=Ambrook+Research+Newsletter&utm_campaign=3f184d5d07-EMAIL_CAMPAIGN_2023_06_26_02_47_COPY_11&utm_medium=email&utm_term=0_-d85bfec4c4-%5BLIST_EMAIL_ID%5D" target="_blank" rel="noopener noreferrer">dairy in public schools</a></li>
         <li><a href="https://www.chriskuang.com/civictech" target="_blank" rel="noopener noreferrer">civic tech resources</a></li>
         <li><a href="https://www.matthewsiu.com/pathfinder" target="_blank" rel="noopener noreferrer">pathfinder</a></li>
+        <li><a href="https://worksinprogress.co/issue/womb-for-improvement/" target="_blank" rel="noopener noreferrer">womb for improvement</a> by aria babu</li>
       </ul>
       
       <br></br>
