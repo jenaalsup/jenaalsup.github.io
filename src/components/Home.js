0800e2d1ba7e797e8be187334833d837fd152a39
jenaalsup <jena.alsup@gmail.com>
2025-12-21 21:44:09 -0800

    update homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,8 @@ function Home() {
       <h3>hi, i'm jena. currently:</h3>
       <ul>
         <li>understanding early human embryos @ <a href="https://zernickagoetzlab.com/" target="_blank" rel="noopener noreferrer">zernicka-goetz lab</a></li>
-        <li>thinking about how to solve poverty, climate, and cities</li>
+        <li>thinking about how to solve poverty, women's health, and reproductive autonomy</li>
         <li>learning about ai, artificial wombs, and plant-based solutions</li>
-        <li>reading <a href="https://www.goodreads.com/book/show/108428.Are_Prisons_Obsolete_" target="_blank" rel="noopener noreferrer">angela davis</a>, <a href="http://www.paulgraham.com/articles.html" target="_blank" rel="noopener noreferrer">paul graham</a>, and <a href="https://www.goodreads.com/book/show/122765395-elon-musk" target="_blank" rel="noopener noreferrer">walter isaacson</a></li>
-        <li>blogging and guiding tours @ <a href="https://studentblog.caltech.edu/author/jenaalsup/" target="_blank" rel="noopener noreferrer">caltech admissions</a></li>
         <li>moving @ <a href="https://www.instagram.com/caltechdance/" target="_blank" rel="noopener noreferrer">caltech dance</a></li>
       </ul>
       
@@ -19,7 +17,6 @@ function Home() {
         <li>automating health plan administration @ <a href="https://yuzu.health/" target="_blank" rel="noopener noreferrer">yuzu health</a></li>
         <li>building security tools <span style={{fontStyle: 'italic'}}>with</span> ai @ <a href="https://www.apple.com/" target="_blank" rel="noopener noreferrer">apple</a></li>
         <li>protecting government communications <span style={{fontStyle: 'italic'}}>from</span> ai @ <a href="https://www.paragonfellowship.org/" target="_blank" rel="noopener noreferrer">paragon policy fellowship</a></li>
-        <li>secretary things @ <a href="https://sites.google.com/site/arccaltech" target="_blank" rel="noopener noreferrer">caltech arc</a></li>
         <li>exposing corporate greenwashing @ <a href="https://www.rmichaelalvarez.com/" target="_blank" rel="noopener noreferrer">alvarez lab</a></li>
         <li>mapping wildfire evacuation demographics @ <a href="https://fse.fsi.stanford.edu/" target="_blank" rel="noopener noreferrer">stanford fse</a></li>
         <li>gutting ecommerce sites @ <a href="https://trove.com/" target="_blank" rel="noopener noreferrer">recurate</a></li>
