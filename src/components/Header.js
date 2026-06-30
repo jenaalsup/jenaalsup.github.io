dce7689a37894f6bafed16274d79391daeac93c5
jenaalsup <jena.alsup@gmail.com>
2026-06-30 22:04:20 +0900

    update location

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,8 @@ function Header({ theme, toggleTheme }) {
       <div className="header-content">
         <h1>Jena<span className="bold">Alsup</span></h1>
         <div className="subtitle-container">
-          <h2>senior @ caltech studying cs</h2>
           <div className="location-line">
-            <span role="img" aria-label="pin">📍</span>sf bay area | pasadena, ca
+            <span role="img" aria-label="pin">📍</span>San Francisco
           </div>
         </div>
       </div>
