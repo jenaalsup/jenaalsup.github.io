17d609ae69a343ff1e90b1ce83f5668c52a7dafd
jenaalsup <jena.alsup@gmail.com>
2026-08-02 13:29:14 -0700

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <a href="mailto:jalsup@caltech.edu">email</a>
-      <a href="https://github.com/jenaalsup" target="_blank" rel="noopener noreferrer">github</a>
-      <a href="https://www.linkedin.com/in/jenaalsup" target="_blank" rel="noopener noreferrer">linkedin</a>
-      <a href="https://www.goodreads.com/jenaalsup" target="_blank" rel="noopener noreferrer">goodreads</a>
+      <a href="mailto:jalsup@caltech.edu">/email</a>
+      <a href="https://github.com/jenaalsup" target="_blank" rel="noopener noreferrer">/github</a>
+      <a href="https://www.linkedin.com/in/jenaalsup" target="_blank" rel="noopener noreferrer">/linkedin</a>
+      <a href="https://www.goodreads.com/jenaalsup" target="_blank" rel="noopener noreferrer">/goodreads</a>
     </footer>
   );
 }
